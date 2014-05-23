35803bd683e8af0662c4aaa2610daf77f2331468
Sedovla96 <Egolior@mail.ru>
2014-05-23 09:51:40 +0300

    console

---
+++ console.cpp
@@ -0,0 +1,68 @@
+#include "console.h"
+
+char win( char field[ ][5] )
+{
+    if( field[0][0] == field[0][2] && field[0][2] == field[0][4] )
+    {
+        return field[0][0];
+    }
+    if( field[2][0] == field[2][2] && field[2][2] == field[2][4] )
+    {
+        return field[2][0];
+    }
+    if( field[4][0] == field[4][2] && field[4][2] == field[4][4] )
+    {
+        return field[4][0];
+    }
+    if( field[0][0] == field[2][0] && field[2][0] == field[4][0] )
+    {
+        return field[0][0];
+    }
+    if( field[0][2] == field[2][2] && field[2][2] == field[4][2] )
+    {
+        return field[0][2];
+    }
+    if( field[0][4] == field[2][4] && field[2][4] == field[4][4] )
+    {
+        return field[0][4];
+    }
+    if( field[0][0] == field[2][2] && field[2][2] == field[4][4] )
+    {
+        return field[0][0];
+    }
+    if( field[0][4] == field[2][2] && field[2][2] == field[4][0] )
+    {
+        return field[0][4];
+    }
+}
+
+bool checkForError( int x, int y )
+{
+    if( x > 3 || y > 3 || x < 1 || y < 1 )
+    {
+        return 0;
+    }
+    return 1;
+}
+
+bool checkFor( int x, int y, char field[ ][ 5 ] )
+{
+    if( field[ 2 * ( y - 1 ) ][ 2 * ( x - 1 ) ] != 'X' && field[ 2 * ( y - 1 ) ][ 2 * ( x - 1 ) ] != 'O' )
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void show ( char field[ ][ 5 ] )
+{
+    for( int i = 0; i < 5; ++i)
+    {
+        for( int j = 0; j < 5; ++j)
+        {
+            cout << field[ i ][ j ];
+        }
+    }
+}
+
+
